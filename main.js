7da461befe28f047ff7e2e74c193f743e258a872
byjucoding <95137527+byjucoding@users.noreply.github.com>
2021-12-11 09:49:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("My_first_project_on_console");
+var first_name = "Ranya";
+var last_name = "Chaudhary";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var a = 10;
+var b = 20;
+var add = a+b;
+console.log(add);
+var sub = a-b;
+console.log(sub);
+var divide = a/b;
+console.log(divide);
